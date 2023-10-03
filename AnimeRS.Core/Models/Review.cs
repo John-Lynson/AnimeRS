3e6ff0b5fc9516d651d3c88111aefcb90d8ea19b
Johnny Graanoogst <Johnlynson809@gmail.com>
2023-10-03 14:25:03 +0200

    -Folders in projecten aangemaakt -Models toegevoegd - UnitTest Project aangemaakt.

---
+++ AnimeRS.Core/Models/Review.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeRS.Core.Models
+{
+    internal class Review
+    {
+        public int Id { get; set; }
+        public int AnimeId { get; set; } // Fkey
+        public int UserId { get; set; } // Fkey
+        public string Comment { get; set; }
+        public int Rating { get; set; }
+        public DateTime PostDate { get; set; }
+    }
+}
