adef825782365ef6d77126dcb162b0718f3f13cb
Johnny Graanoogst <Johnlynson809@gmail.com>
2023-11-23 09:26:07 +0100

    Adds

--- AnimeRS.Core/Models/AnimeRating.cs
+++ AnimeRS.Core/Models/AnimeRating.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace AnimeRS.Core.Models
 {
-    internal class AnimeRating
+    public class AnimeRating
     {
+        public int Id { get; private set; }
+        public int AnimeId { get; private set; }
+        public int AnimeLoverId { get; private set; }
+        public int Rating { get; private set; }
+
+        public AnimeRating(int animeId, int animeLoverId, int rating)
+        {
+            AnimeId = animeId;
+            AnimeLoverId = animeLoverId;
+            Rating = rating;
+        }
+
     }
+
 }
--- AnimeRS.Core/Models/UserProfile.cs
+++ AnimeRS.Core/Models/UserProfile.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace AnimeRS.Core.Models
 {
-    internal class UserProfile
+    public class UserProfile
     {
+        public int Id { get; private set; }
+        public int AnimeLoverId { get; private set; }
+        public string FavoriteGenre { get; private set; }
+        public List<int> WatchedAnimeIds { get; private set; }
+
+        public UserProfile(int animeLoverId, string favoriteGenre, List<int> watchedAnimeIds)
+        {
+            AnimeLoverId = animeLoverId;
+            FavoriteGenre = favoriteGenre;
+            WatchedAnimeIds = watchedAnimeIds ?? new List<int>();
+        }
     }
 }
