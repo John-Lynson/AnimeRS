c7b1ae09409d9f778f0789590658a70c59ce7f51
Johnny Graanoogst <Johnlynson809@gmail.com>
2024-01-04 00:31:54 +0100

    Detailpage

--- AnimeRS/Controllers/SearchController.cs
+++ AnimeRS/Controllers/SearchController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using AnimeRS.Core.Services; // Zorg ervoor dat u de juiste namespaces importeert
 
 namespace AnimeRS.Web.Controllers
 {
     public class SearchController : Controller
     {
+        private readonly AnimeService _animeService;
+
+        public SearchController(AnimeService animeService)
+        {
+            _animeService = animeService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        [Route("anime/{id}")]
+        public IActionResult AnimeDetails(int id)
+        {
+            var anime = _animeService.GetAnimeById(id);
+            if (anime == null)
+            {
+                return NotFound();
+            }
+            return View(anime);
         }
     }
 }
